b5574fa8d56fd266f673687a5423031e215f251a
Vasilkova Yuliya <2st.vasilkou@gmail.com>
2023-02-07 15:20:07 +0300

    Первое задание изменено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,45 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
-  return arr;
-}
+   let oneRoot;
+  let twoRoot;
+  let d = Math.pow(b, 2) - 4 * a * c;
+  
+ 
+  if (d === 0) {
+    oneRoot = -b/(2*a);
+    arr.push(oneRoot);
+    console.log(arr);
+  } else if (d > 0) {
+    oneRoot = (-b + Math.sqrt(d) )/(2*a);
+    twoRoot = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(oneRoot);
+    arr.push(twoRoot);
+    console.log("Первый корень =" + arr[0] + "," + "Второй корень =" + arr[1])  
+  }
+   return arr;
+  }
+  
+  solveEquation(1, 2, 3);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthPercent;
+  let bodyCredit;
+  let monthlyPayment;
+  let totalSum;
+
+   if (isNaN(percent) ||
+     isNaN(contribution) ||
+     isNaN(amount) ||
+     isNaN(countMonths)) 
+    { 
+    return false;
+    }
+   
+  monthPercent = percent / 100 / 12;
+  bodyCredit = amount - contribution;
+  monthlyPayment = bodyCredit * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+  totalSum = parseFloat((monthlyPayment * countMonths).toFixed(2));
+  return totalSum;
 }
+calculateTotalMortgage(10, 0, 50000, 12);
