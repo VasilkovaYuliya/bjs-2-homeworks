36cf0580fb8a97ef8cabe5c66cf80a63e27ca274
Vasilkova Yuliya <2st.vasilkou@gmail.com>
2023-03-03 16:32:53 +0300

    Четвертое задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+};
+
+let first = new Student("Аня", "женский", 26);
+let second = new Student("Миша", "мужской", 33);
+let third = new Student("Соня", "женский", 29);
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
+first.setSubject('Физика');
+
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks === undefined){ 
+    console.log('Студент отчислен');
+    return;
+    } 
+    this.marks.push(...marksToAdd);
 }
+first.addMarks(5,3,9);
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let mark of this.marks){
+    sum += mark;
+  }
+  return sum / this.marks.length;
 }
+first.getAverage();
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
+first.exclude();
