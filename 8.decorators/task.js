543ac3486b4281738e22601d28fda09dea1f5df8
Vasilkova Yuliya <2st.vasilkou@gmail.com>
2023-04-10 14:37:48 +0300

    Восьмое задание.

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,68 +1,49 @@
 //Задача № 1
-import md5 from 'js-md5';
-
 function cachingDecoratorNew(func) {
-let cache = [];
-return function wrapper (...args) {
-const hash = String(md5(args));
-let objectInCache = cache.find((item) => item.hash === hash);
-    if (objectInCache) {
-        console.log("Из кэша: " + `{objectInCache.value}`);
-        return objectInCache.value;
-    }
+	let cache = [];
+	function wrapper(...args) {
+		const hash = md5(args);
+		let objectInCache = cache.find((item) => item.hash === hash);
+		if (objectInCache) {
+			console.log(`"Из кэша: " ${objectInCache.value}`);
+			return objectInCache.value;
+		}
 
-    const result = func(...args);
-    cache.push({hash, value: result});
-    if (cache.length > 5) {
-    cache.shift();
-    }
-    console.log(`Вычисляем: ${result}`);
-    return result;
-  }
+		let result = func(...args);
+		cache.push({
+			hash,
+			value: result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log(`Вычисляем: ${result}`);
+		return result;
+	}
+	return  wrapper;
 }
   
-let add2 = (a, b) => a + b;
-let multiply3 = (a, b, c) => a * b * c;
-let upgAdd2;
-let upgMultiply3;
-
-upgAdd2 = cachingDecoratorNew(add2);
-upgMultiply3 = cachingDecoratorNew(multiply3);
-
-upgAdd2(1,2)
-upgAdd2(1,2)
-upgAdd2(1,2)
-
-
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-let timeOut = null;
-let count = 0;
-let allCount = 0;
+	let timeoutId;
+	let count = 0;
+	let allCount = 0;
 
-function wrapper (...args) {
-  allCount++;
-  if (!timeOut) {
-      count++;
-  func.apply(this, args);
+	function wrapper(...args) {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
 
-  
-} else{
-  clearTimeout(timeOut);
+		timeoutId = setTimeout(() => {
+			const result = func.apply(this, args);
+			wrapper.count = count++;
+			wrapper.allCount = allCount++;
+			return result;
+		}, delay);
+	}
+
+	wrapper.count = count;
+	wrapper.allCount = allCount;
+
+	return wrapper;
 }
-
-timeOut = setTimeout(() => {
-  timeOut = null;
-}, delay)
-}
-
-wrapper.count = function() {
-  return count;
-}
-
-wrapper.allCount = function() {
-  return allCount;
-}
-return wrapper;
-}
-
